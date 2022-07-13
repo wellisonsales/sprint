aed6e785ee73264bd9e14ac1851bd313155b8c39
Wellison Sales <wellisonsalesbr@gmail.com>
2022-07-13 12:30:51 -0300

    feat: quest alinhamento

--- sprint.js
+++ sprint.js
@@ -5,4 +5,7 @@ let propriedadeCss = readlineSync.question("digite uma propriedade CSS:");
 console.log("você digitou " + propriedadeCss);
 
 let fontSize = readlineSync.question("ok, agora digite um valor para sua fonte:");
-console.log("O tamanho da sua fonte será " + fontSize)
+console.log("O tamanho da sua fonte será " + fontSize);
+
+let textAlign = readlineSync.question("otimo tamanho, agora, digite o alinhamento do texto: ");
+console.log("O alinhamento será " + textAlign);
