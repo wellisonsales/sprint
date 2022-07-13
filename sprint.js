c9b0631baa429119d806ce20c909c4054ee4631f
Wellison Sales <wellisonsalesbr@gmail.com>
2022-07-13 11:24:42 -0300

    feat: testando código com while

--- sprint.js
+++ sprint.js
@@ -1,3 +1,16 @@
 
-import readlineSync from "readlineSync"
+import readlineSync from "readline-sync";
+
+let propriedades = [];
+
+while (true) {
+    
+    const input = readlineSync.question('Qual seu nome?');
+
+    if (!nome){
+        console.log ("digite um valor,por favor.");
+        return;
+    }
+ 
+}
 
